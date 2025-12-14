7ced244bac6299331b961f4ed56e6405c94e6bfa
Eduardo Augusto Lima Pereira <42621512+Edd002@users.noreply.github.com>
2025-12-14 14:55:25 -0300

    #Fase-4-ajuste-time-zone : Ajuste de time zone para response

--- src/main/java/fiap/tech/challenge/online/course/feedback/deliver/serverless/payload/record/FeedbackResponse.java
+++ src/main/java/fiap/tech/challenge/online/course/feedback/deliver/serverless/payload/record/FeedbackResponse.java
@@ -1,6 +1,9 @@
 package fiap.tech.challenge.online.course.feedback.deliver.serverless.payload.record;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import fiap.tech.challenge.online.course.feedback.deliver.serverless.payload.enumeration.AssessmentType;
 
-public record FeedbackResponse(Boolean urgent, String description, String comment, String teacherName, String teacherEmail, String studentName, String studentEmail, String assessmentName, AssessmentType assessmentType, Double assessmentScore, String createdIn) {
+import java.util.Date;
+
+public record FeedbackResponse(Boolean urgent, String description, String comment, String teacherName, String teacherEmail, String studentName, String studentEmail, String assessmentName, AssessmentType assessmentType, Double assessmentScore, @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy hh:mm", timezone = "America/Sao_Paulo") Date createdIn) {
 }
